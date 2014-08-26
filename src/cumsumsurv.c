e8d672eacc1d28a27cc5000532576739285a0ee5
Mai Zhou <mai@ms.uky.edu>
2014-08-26 00:00:00 +0000

    version 0.9-9-6

--- src/cumsumsurv.c
+++ src/cumsumsurv.c
@@ -179,7 +179,7 @@ for (;i<L;i++) re+=wt[i]*(x[i]-mu)/(allw+lambda*(x[i]-mu));
 return(re);
 }
 
-inline double summm(double *x,int L){
+static inline double summm(double *x,int L){
 int i=0;double re=0.;
 for (;i<L;i++){
 re+=x[i];
@@ -187,7 +187,7 @@ re+=x[i];
 return(re);
 }
 
-inline double maxabs(double *x, double mu,int L){
+static inline double maxabs(double *x, double mu,int L){
 int i=0;double re=fabs(x[0]-mu);
 for(;i<L;i++){
 if (fabs(x[i]-mu)>re) re=fabs(x[i]-mu);
